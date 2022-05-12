0838568b4b2fcbbc8b25f609c5a662b410439270
Ervin Natareno <ean1207@gmail.com>
2022-05-11 18:17:00 -0700

    Npm Run Build Fix

--- js/javascript.js
+++ js/javascript.js
@@ -1,6 +1,6 @@
 window.addEventListener('scroll', function() {
     var nav = document.getElementsByTagName('nav')[0];
-    let windowPosition = window.scrollY > 200
+    var windowPosition = window.scrollY > 200
 
     nav.classList.toggle('scrolling-active', windowPosition);
 })
